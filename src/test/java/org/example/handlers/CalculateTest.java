289d2c64c142b0e0fba1963a26e59a4b98c2d899
Nasty Z <zlatnasty20@gmail.com>
2024-01-16 10:59:00 +0300

    CalculateTest (Mockito)

--- src/test/java/org/example/handlers/CalculateTest.java
+++ src/test/java/org/example/handlers/CalculateTest.java
@@ -1,26 +1,31 @@
 package org.example.handlers;
 
-import org.example.argument_providers.ExecuteArgumentsProvider;
 import org.example.models.Lexeme;
+import org.example.models.LexemeType;
 import org.example.services.LexemeAnalysis;
 import org.example.services.LexemeBuffer;
 import org.example.services.RecursiveCalculation;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CalculateTest {
-    @Mock
+    @Spy
     private RecursiveCalculation recursiveCalculation;
     @Mock
     private LexemeAnalysis lexemeAnalysis;
@@ -28,11 +33,37 @@ class CalculateTest {
     private Calculate calculate;
 
     @ParameterizedTest
-    @ArgumentsSource(ExecuteArgumentsProvider.class)
-    void execute(List<Lexeme> lexemeList, String expression, double expected) {
-        when(lexemeAnalysis.analysis(expression)).thenReturn(lexemeList);
-        when(recursiveCalculation.expr(any(LexemeBuffer.class))).thenReturn(expected);
-        double actual = calculate.execute(expression);
-        assertEquals(expected, actual);
+    @MethodSource("generate")
+    void execute(List<Lexeme> lexemeList) {
+        String expression = "(2+2)*2/2";
+        double expected = 4.0;
+        when(lexemeAnalysis.analysis(anyString())).thenReturn(lexemeList);
+
+        assertEquals(expected, calculate.execute(expression));
+
+        verifyNoMoreInteractions(lexemeAnalysis);
+        verify(recursiveCalculation, never()).lambdaOperations(LexemeType.MINUS);
+        verify(recursiveCalculation, atLeastOnce()).factor(any(LexemeBuffer.class));
+        verify(recursiveCalculation, atLeastOnce()).plusMinus(any(LexemeBuffer.class));
+        verify(recursiveCalculation, atLeastOnce()).mulDiv(any(LexemeBuffer.class));
+        verify(recursiveCalculation, atLeastOnce()).lambdaOperations(any(LexemeType.class));
+    }
+
+    private static Stream<Arguments> generate() {
+        return Stream.of(
+                Arguments.of(new ArrayList<>(List.of(
+                                new Lexeme(LexemeType.OPEN_BRACKET),
+                                new Lexeme(LexemeType.NUMBER, "2"),
+                                new Lexeme(LexemeType.PLUS),
+                                new Lexeme(LexemeType.NUMBER, "2"),
+                                new Lexeme(LexemeType.CLOSE_BRACKET),
+                                new Lexeme(LexemeType.MULTIPLY),
+                                new Lexeme(LexemeType.NUMBER, "2"),
+                                new Lexeme(LexemeType.DIVIDE),
+                                new Lexeme(LexemeType.NUMBER, "2"),
+                                new Lexeme(LexemeType.EOF)
+                        ))
+                )
+        );
     }
 }
