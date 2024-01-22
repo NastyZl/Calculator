5de02c63f15471833d4c885151090ab42f4e1061
Nasty Z <nastyzlat20@gmail.com>
2024-01-22 19:23:02 +0300

    using mockito

--- src/test/java/org/example/handlers/CalculateTest.java
+++ src/test/java/org/example/handlers/CalculateTest.java
@@ -2,6 +2,7 @@ package org.example.handlers;
 
 import org.example.argument_providers.ExecuteArgumentsProvider;
 import org.example.models.Lexeme;
+import org.example.models.LexemeType;
 import org.example.services.LexemeAnalysis;
 import org.example.services.LexemeBuffer;
 import org.example.services.RecursiveCalculation;
@@ -10,17 +11,20 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CalculateTest {
-    @Mock
+    @Spy
     private RecursiveCalculation recursiveCalculation;
     @Mock
     private LexemeAnalysis lexemeAnalysis;
@@ -29,10 +33,13 @@ class CalculateTest {
 
     @ParameterizedTest
     @ArgumentsSource(ExecuteArgumentsProvider.class)
-    void execute(List<Lexeme> lexemeList, String expression, double expected) {
+    void executeTest(List<Lexeme> lexemeList, String expression, double expected) {
         when(lexemeAnalysis.analysis(expression)).thenReturn(lexemeList);
         when(recursiveCalculation.expr(any(LexemeBuffer.class))).thenReturn(expected);
         double actual = calculate.execute(expression);
         assertEquals(expected, actual);
+        verify(recursiveCalculation).lambdaOperations(LexemeType.PLUS);
+        verify(recursiveCalculation).expr(any(LexemeBuffer.class));
+
     }
 }
